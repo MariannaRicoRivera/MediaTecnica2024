1001cab4f87d3c3b1ed87a3af652a95b9b0891a1
MariannaRico <mariannarico1808@gmail.com>
2024-04-02 10:43:58 -0500

    primera parte

---
+++ Clase2/data/data.js
@@ -0,0 +1,12 @@
+export const pinturas =[
+
+
+{
+    id:1,
+    name : "La Noche Estrellada, Van Gogh",
+    Image:"./imagenes/imagen1.jpg"
+}
+
+
+
+]
---
+++ Clase2/index.js
@@ -0,0 +1,6 @@
+import { pinturas } from "./data/data.js";
+ console.log(pinturas);
+
+ const items = document.getElementById ('Items');
+ const templateCard = document.getElementById ('template-card')
+ const fragment = document.createDocumentFragment();
