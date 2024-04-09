bb2b18ee30dbed815f025dd2c72e6f9fa7bc143a
MariannaRico <mariannarico1808@gmail.com>
2024-04-09 09:17:52 -0500

    Ejercicio finalizado

--- Clase2/data/data.js
+++ Clase2/data/data.js
@@ -5,7 +5,17 @@ export const pinturas =[
     id:1,
     name : "La Noche Estrellada, Van Gogh",
     Image:"./imagenes/imagen1.jpg"
-}
+},
+{
+    id:1,
+    name : "La Noche Estrellada, Van Gogh",
+    Image:"./imagenes/imagen1.jpg"
+},
+{
+    id:1,
+    name : "La Noche Estrellada, Van Gogh",
+    Image:"./imagenes/imagen1.jpg"
+},
 
 
 
